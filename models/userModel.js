0d5e1d30e0e2f3616c340d8136d8c39308ac8f91
dy222175 <108139948+dy222175@users.noreply.github.com>
2024-07-13 19:05:18 +0530

    initial commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,32 @@
-import mongoose from "mongoose";
-import validator from "validator";
-import bcrypt from "bcryptjs";
-import JWT from "jsonwebtoken";
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcryptjs'
+import JWT from 'jsonwebtoken'
 
 //schema
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: [true, "First Name is Required!"],
+      required: [true, 'First Name is Required!'],
     },
     lastName: {
       type: String,
-      required: [true, "Last Name is Required!"],
+      required: [true, 'Last Name is Required!'],
     },
     email: {
       type: String,
-      required: [true, " Email is Required!"],
+      required: [true, ' Email is Required!'],
       unique: true,
       validate: validator.isEmail,
     },
     password: {
       type: String,
-      required: [true, "Password is Required!"],
-      minlength: [6, "Password length should be greater than 6 character"],
+      required: [true, 'Password is Required!'],
+      minlength: [6, 'Password length should be greater than 6 character'],
       select: true,
     },
-    accountType: { type: String, default: "seeker" },
+    accountType: { type: String, default: 'seeker' },
     contact: { type: String },
     location: { type: String },
     profileUrl: { type: String },
@@ -34,29 +34,29 @@ const userSchema = new mongoose.Schema(
     jobTitle: { type: String },
     about: { type: String },
   },
-  { timestamps: true }
-);
+  { timestamps: true },
+)
 
 // middelwares
-userSchema.pre("save", async function () {
-  if (!this.isModified) return;
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+userSchema.pre('save', async function () {
+  if (!this.isModified) return
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 //compare password
 userSchema.methods.comparePassword = async function (userPassword) {
-  const isMatch = await bcrypt.compare(userPassword, this.password);
-  return isMatch;
-};
+  const isMatch = await bcrypt.compare(userPassword, this.password)
+  return isMatch
+}
 
 //JSON WEBTOKEN
 userSchema.methods.createJWT = function () {
   return JWT.sign({ userId: this._id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "1d",
-  });
-};
+    expiresIn: '1d',
+  })
+}
 
-const Users = mongoose.model("Users", userSchema);
+const Users = mongoose.model('Users', userSchema)
 
-export default Users;
+export default Users
